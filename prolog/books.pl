f96ec1ec9adb00df3b554ff4c9deef95ca9adbf6
Robin Veeck <rveeck@slack-corp.com>
2018-09-30 13:37:20 -0700

    day one prolog

---
+++ prolog/books.pl
@@ -0,0 +1,5 @@
+wrote(michael_chabon, amazing_adventures_of_kavalier_and_clay).
+wrote(michael_chabon, moonglow).
+wrote(michael_chabon, summerland).
+wrote(joan_didion, blue_nights).
+wrote(joan_didion, slouching_toward_bethlehem).
